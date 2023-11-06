ab549b2037d710a4500ce6d7c176848a834e4da2
Guilherme Souza <gui.brito.souza@gmail.com>
2023-11-06 17:25:22 +0000

    Pratique finalizado

--- pratique_EBAC/main.js
+++ pratique_EBAC/main.js
@@ -1,5 +1,24 @@
-$('form').on('submit', function(e){
-    e.preventDefault();
-
-    
-})
+$(document).ready(function() {
+    // Seleciona o botão "TO ADD" e adiciona um evento de clique
+    $("button:contains('TO ADD')").click(function() {
+      // Captura o valor do campo de entrada
+      var taskText = $("input[type='text']").val();
+  
+      // Verifica se o campo de entrada não está vazio
+      if (taskText !== '') {
+        // Cria um novo elemento de lista com o texto da tarefa
+        var listItem = $("<li>").text(taskText);
+  
+        // Adiciona um manipulador de evento de clique para alternar a classe "completed"
+        listItem.click(function() {
+          $(this).toggleClass("completed");
+        });
+  
+        // Adiciona o novo elemento de lista à lista ordenada
+        $("ol").append(listItem);
+  
+        // Limpa o campo de entrada após adicionar a tarefa
+        $("input[type='text']").val('');
+      }
+    });
+  });
