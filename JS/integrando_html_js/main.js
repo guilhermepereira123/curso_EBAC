21d6c67179399977f435b20664bb8a9682d53bf9
Guilherme Souza <gui.brito.souza@gmail.com>
2023-11-20 14:29:27 +0000

    integrando html com js

--- JS/integrando_html_js/main.js
+++ JS/integrando_html_js/main.js
@@ -1,12 +1,20 @@
+const form = document.getElementById('form-deposito')
 
-
-
-function depositou(){
-    let nome = document.getElementById('name').value
-    let conta = document.getElementById('conta').value
-    let valor = document.getElementById('valor').value
-    
-    res.innerHTML = (`${nome} depositou ${valor}€ na conta ${conta}`)
-    
-    return false;
+function validaNome(nomeCompleto){
+    const nomeComoArray = nomeCompleto.split(' ');
+    return nomeComoArray.length >= 2;
 }
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+
+    const nome = document.getElementById('name')
+    const conta = document.getElementById('conta').value
+    const valor = document.getElementById('valor').value
+    
+    if(validaNome(nome.value)){
+        res.innerHTML = `Nome: ${nome.value} <br> Conta: ${conta} <br> Valor: ${valor}` 
+    }else {
+        alert('[ERRO]')
+    } 
+})
